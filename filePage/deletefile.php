8640fdb880991216c11b41eb7ef392a057b62a3a
khiem20tc <53548789+khiem20tc@users.noreply.github.com>
2020-06-24 00:03:35 +0700

    Done task: Up, delete file

---
+++ filePage/deletefile.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+<style>
+    * {
+        font-family: sans-serif;
+        /* Change your font family */
+    }
+
+    .title {
+        margin-left: 130px;
+    }
+
+    .content-table {
+        border-collapse: collapse;
+        margin: 25px 0;
+        font-size: 0.9em;
+        min-width: 400px;
+        border-radius: 5px 5px 0 0;
+        overflow: hidden;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+    }
+
+    .content-table thead tr {
+        background-color: #009879;
+        color: #ffffff;
+        text-align: left;
+        font-weight: bold;
+    }
+
+    .content-table th,
+    .content-table td {
+        padding: 12px 15px;
+    }
+
+    .content-table tbody tr {
+        border-bottom: 1px solid #dddddd;
+    }
+
+    .content-table tbody tr:nth-of-type(even) {
+        background-color: #f3f3f3;
+    }
+
+    .content-table tbody tr:last-of-type {
+        border-bottom: 2px solid #009879;
+    }
+
+    .btn{
+    text-decoration: none;
+    color: white;
+    background-color: #e6760e;
+    padding: 15px 30px;
+    border-radius: 20px;
+    margin: 0 5px;
+    width: 60px;
+    display: inline-block;
+    text-align: center;
+    transition: all .3s;
+}
+</style>
+
+<body>
+
+    <body>
+	<?php
+    include "config.php";
+    $connect = new config();
+	// $username = 'root'; // Khai báo username
+    //   $password = '';      // Khai báo password
+    //   $server   = 'localhost';   // Khai báo server
+    //   $dbname   = "file";      // Khai báo database
+      
+    //   $connect = new mysqli($server, $username, $password, $dbname);
+    //   if ($connect->connect_error) {
+	// 	  echo ("fail");
+    //     die("Không kết nối :" . $connect->connect_error);
+    //     exit();
+    // }
+	$sql = "SELECT * FROM file";
+	//$result = $connect->query($sql);
+    $result = $connect->traverse($sql);
+
+	?>
+        <h1 class="title">
+            Tài liệu 
+        </h1>
+        <table class="content-table">
+            <thead>
+                <tr>
+                    <th>Tài liệu</th>
+                    <th>Date</th>
+					<th></th>
+                </tr>
+            </thead>
+            <?php //while($row = $result->fetch_assoc())
+            foreach($result as $row)
+         {
+	//$row = $result->fetch_assoc(); 
+	?>
+            <tbody>
+                <tr>
+                <td>
+                    <a href=<?php echo $row["url"]; ?>><?php echo $row["file"];?></a>
+                </td>
+                <td>
+                      <?php echo $row["date"];?>
+                </td>
+				<td>
+                    <a href="delete.php?url=<?php echo $row['url'];?>" target="_blank" class="delete">xóa</a>
+					<!--<button type = "submit" class="delete" method="post">xóa</button> -->
+                </td>
+                </tr>
+            </tbody>
+		 <?php } ?>
+            </table>
+    </body>
+	 <?php
+//Đóng database
+//$connect->close();
+    ?>
+    <!-- <a href="" class="btn">Chỉnh</a>
+    <a href="uploadfile.php" class="btn">Tải lên</a> 
+    <a href="" class="btn">Xóa</a> -->
+</html>
---
+++ filePage/uploadfile.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form action="" method="POST" enctype="multipart/form-data">
+        <input type="file" name="fileUpload" value="">
+		<br>
+		<input type="text" name="fileName" placeholder="Nhập tên file">
+        <br>
+        <input type="submit" value="Upload" name="up">
+</form>
+<?php
+if (isset($_POST['up']) && isset($_FILES['fileUpload'])) {
+    if ($_FILES['fileUpload']['error'] > 0)
+        echo "Upload lỗi rồi!";
+    else {
+        move_uploaded_file($_FILES['fileUpload']['tmp_name'], '../uploads/' . $_FILES['fileUpload']['name']);
+        echo "upload thành công <br/>";
+        echo 'Dường dẫn: uploads/' . $_FILES['fileUpload']['name'] . '<br>';
+        echo 'Loại file: ' . $_FILES['fileUpload']['type'] . '<br>';
+        echo 'Dung lượng: ' . ((int)$_FILES['fileUpload']['size'] / 1024) . 'KB';
+    }
+	// $username = 'root'; // Khai báo username
+    //   $password = '';      // Khai báo password
+    //   $server   = 'localhost';   // Khai báo server
+    //   $dbname   = "file";      // Khai báo database
+      
+    //   $connect = new mysqli($server, $username, $password, $dbname);
+    //   if ($connect->connect_error) {
+	// 	  echo ("fail");
+    //     die("Không kết nối :" . $connect->connect_error);
+    //     exit();
+    // }
+	
+	//$file = $_FILES['fileUpload']['name'];
+	$file = $_POST['fileName'];
+	$url = '../uploads/' . $_FILES['fileUpload']['name'];
+	$date = "" . date("d/m/Y");
+    echo $date;
+    
+    include "config.php";
+    $connect = new config();
+	
+	 $sql = "INSERT INTO file (file,url,date)
+      VALUES ('$file','$url','$date')";
+	  
+      if ($connect->traverseFunc($sql) === TRUE) {
+        echo ("Thêm dữ liệu thành công");
+    } // else {
+    //     echo "Error: " . $sql . "<br>" . $connect->error;
+    // }
+}
+
+
+?>
+</body>
+</html>

3b04fbc827e6e78b979979bbb3b24b1ae69e0601
dinhkhanh412 <dinhkhanh412@gmail.com>
2020-06-29 10:27:37 +0700

    add button

--- createExamPage/index.php
+++ createExamPage/index.php
@@ -4,11 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript" src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML' async></script>
     <title>Create Exam</title>
     <style>
         <?php
         include 'css/style.css';
+        include 'js/main.js';
         include 'controller.php';
+        include 'readjson.php'
         ?>
     </style>
 
@@ -18,48 +21,52 @@
 
 <body>
 
-    <form action="" method="POST">
-        <label id="questionLabel" for=Question>Câu hỏi</label>
-        <label id="questionOrder" for=Question> <?php global $idx; echo $idx++; ?> </label><br>
-        <textarea id="question" name="question"></textarea><br><br>
+    <label id="questionLabel" for=Question>Câu hỏi </label>
+    <label id="questionOrder">1</label><br>
+    <textarea id="question" name="question"></textarea><br><br>
 
-        <label for=ansE>Câu A:</label><br>
-        <input type="text" id="ansA" name="ansA"><br><br>
+    <label for=ansE>Câu A:</label><br>
+    <input type="text" id="ansA" name="ansA"><br><br>
 
-        <label for=ansA>Câu B:</label><br>
-        <input type="text" id="ansB" name="ansB"><br><br>
+    <label for=ansA>Câu B:</label><br>
+    <input type="text" id="ansB" name="ansB"><br><br>
 
-        <label for=ansB>Câu C:</label><br>
-        <input type="text" id="ansC" name="ansC"><br><br>
+    <label for=ansB>Câu C:</label><br>
+    <input type="text" id="ansC" name="ansC"><br><br>
 
-        <label for=ansC>Câu D:</label><br>
-        <input type="text" id="ansD" name="ansD"><br><br>
+    <label for=ansC>Câu D:</label><br>
+    <input type="text" id="ansD" name="ansD"><br><br>
 
-        <label>Đáp án:</label><br>
+    <label>Đáp án:</label><br>
 
-        <select name="key" id="key">
-            <option value="A">A</option>
-            <option value="B">B</option>
-            <option value="C">C</option>
-            <option value="D">D</option>
-        </select> 
-        <br><br>
-
-        <input type="submit" value="Submit" />
-    </form>
+    <select name="key" id="key">
+        <option value="A">A</option>
+        <option value="B">B</option>
+        <option value="C">C</option>
+        <option value="D">D</option>
+    </select>
+    <br><br>
 
 
+    <button id="Next" class="item_button" type="Button" onclick="next()">Next</button>
 
-    <?php   
-        $question = $_POST['question'];
-        $ansA     = $_POST['ansA'];
-        $ansB     = $_POST['ansB'];
-        $ansC     = $_POST['ansC'];
-        $ansD     = $_POST['ansD'];
-        $key      = $_POST['key'];
+    <button id="Previous" class="item_button" type="Button" onclick="previous()">Previous</button><br> <br>
     
-        $exam[0] = new Question("Câu ".$idx, $question, $ansA, $ansB, $ansC, $ansD, $key);
-        $exam[0]->__print();
+
+    <input type="submit" value="Submit" /> 
+    <?php
+    $temp;
+    //readJson("read.json", $temp);
+
+    $question = $_POST['question'];
+    $ansA     = $_POST['ansA'];
+    $ansB     = $_POST['ansB'];
+    $ansC     = $_POST['ansC'];
+    $ansD     = $_POST['ansD'];
+    $key      = $_POST['key'];
+
+    $exam[0] = new Question("Câu " . $idx, $question, $ansA, $ansB, $ansC, $ansD, $key);
+    $exam[0]->__print();
     ?>
 
 </body>

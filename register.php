4326668f225d89eb31bec21e2a2c11557878e9b3
khiem20tc <53548789+khiem20tc@users.noreply.github.com>
2020-07-26 20:32:15 +0700

    update CSS in sign_in, sign_up

--- register.php
+++ register.php
@@ -1,23 +1,47 @@
+<link rel="stylesheet" href="/GoPro_SE192/assets/css/style.css" />
 <?php
 //signup.php
 include 'userDB.php';
 include 'header.php';
-echo '<h3>Sign up</h3>';
- 
+
 if($_SERVER['REQUEST_METHOD'] != 'POST')
 {
     /*the form hasn't been posted yet, display it
       note that the action="" will cause the form to post to the same page it is on */
+    // echo '<form method="post" action="">
+    //     Username: <input type="text" name="user_name" placeholder="Username" /> <br>
+    //     Password: <input type="password" name="user_pass" placeholder="Password"> <br>
+    //     Password again: <input type="password" name="user_pass_check" placeholder="Password again"> <br>
+    //     Email <input type="email" name="user_email" placeholder="Email@"> <br>
+    //     Role <br>
+    //     <input type="radio" name="user_role" value="Student" checked> Student <br>
+    //     <input type="radio" name="user_role" value="Teacher">Teacher <br>
+    //     <input type="submit" value="Submit" />
+    //  </form>';
+
     echo '<form method="post" action="">
-        Username: <input type="text" name="user_name" placeholder="Username" /> <br>
-        Password: <input type="password" name="user_pass" placeholder="Password"> <br>
-        Password again: <input type="password" name="user_pass_check" placeholder="Password again"> <br>
-        Email <input type="email" name="user_email" placeholder="Email@"> <br>
-        Role <br>
-        <input type="radio" name="user_role" value="Student" checked> Student <br>
-        <input type="radio" name="user_role" value="Teacher">Teacher <br>
-        <input type="submit" value="Submit" />
-     </form>';
+         <div class="joinOuterContainer">
+         <div class="joinInnerContainer">
+           <h1 class="heading">SIGN UP</h1>
+           <div>
+             <input placeholder="Username" class="joinInput" type="text" name="user_name" />
+           </div>
+           <div>
+             <input placeholder="Password" class="joinInput mt-20" type="password" name="user_pass"/>
+           </div>
+           <div>
+             <input placeholder="Password again" class="joinInput mt-20" type="password" name="user_pass_check"/>
+           </div>
+           <div> 
+           <input type="email" class="joinInput mt-20" name="user_email" placeholder="Email@"> <br>
+           Role <br>
+           <input type="radio" name="user_role" value="Student" checked> Student <br>
+           <input type="radio" name="user_role" value="Teacher">Teacher <br>
+           </div>
+           <input class="button mt-20" type="submit" value="Sign in"></input>
+         </div>
+       </div>
+       </form>';
 }
 else
 {

5e7094b7741e456f58b8a3c267adff8b3bd389a4
Ankun <sytungan@gmail.com>
2020-07-27 19:33:38 +0700

    Fix marking v1

--- testExamPage/view/resultForm.php
+++ testExamPage/view/resultForm.php
@@ -1,10 +1,10 @@
 <?php
 $result = $viewExam->ctrl->markingExam($_GET['subject'], $viewExam->listKey, $_POST);
-echo "$result";
+echo "<div id='testResult'>";
+echo "<h4>".$result."</h4>";
 echo "<br>";
 echo "<a href='viewSuggestion.php'>Xem gợi ý</a><br>";
 echo "<a href='/GoPro_SE192/testExamPage/testExam.php'>Tiếp tục thi thử</a><br>";
 echo "<a href='/GoPro_SE192/index.php'>Về trang chủ</a><br>";
-
-
+echo "</div>";
 ?>

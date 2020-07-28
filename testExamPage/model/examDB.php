d119732c5b86f6bf0a7f42090745976c678f25d7
Ankun <sytungan@gmail.com>
2020-07-28 19:38:05 +0700

    Update CSS for all

--- testExamPage/model/examDB.php
+++ testExamPage/model/examDB.php
@@ -5,8 +5,20 @@ class examDB {
     public function __construct() {
         $this->conn = new config();
     }
+    public function countList($dbName, $title, $value) {
+        $sql= "SELECT COUNT(*) as 'total' FROM $dbName WHERE $title='$value'";
+        $result = $this->conn->traverse($sql);
+        foreach ($result as $row) {
+            $total = $row['total'];
+            break;
+        }
+        return $total;
+    }
     public function getListExam($_subject) {
-        return $this->conn->traverse("SELECT * FROM examDB WHERE subject='$_subject'");
+        if ($this->countList('examDB', 'subject', $_subject) > 0) {
+            return $this->conn->traverse("SELECT * FROM examDB WHERE subject='$_subject'");
+        } 
+        else return null;   
     }    
     public function getExamByID($_id) {
         return $this->conn->traverse("SELECT * FROM examDB WHERE id='$_id'");

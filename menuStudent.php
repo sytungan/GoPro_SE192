021e7b29207b2a1da16c2291cb257c7e5e901389
Ankun <sytungan@gmail.com>
2020-07-28 22:00:27 +0700

    Done task: Menu for 3 actors

--- menuStudent.php
+++ menuStudent.php
@@ -1,15 +1,9 @@
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="assets/css/style.css" />
-</head>
-<div id="menu">
-    <ul id="main_menu">
-        <li> <a href="../index.php">Trang chủ</a> </li>
-        <li> <a href="weeklyTest.php">Đề thi tuần</a></li>
-        <li> <a href="/GoPro_SE192/testExamPage/testExam.php">Thi thử</a></li>
-        <li> <a href="schedule.php">Thời khóa biểu</a></li>
-        <li> <a href="document.php">Tài liệu</a></li>
-        <li> <a href="news.php">Tin tức</a></li>
-        <li> <a href="topic.php">Diễn đàn</a></li>
-    </ul>
-</div>
+<ul id="main_menu">
+    <li> <a href="/GoPro_SE192/index.php">Trang chủ</a> </li>
+    <li> <a href="/GoPro_SE192/weeklyTest.php">Đề thi tuần</a></li>
+    <li> <a href="/GoPro_SE192/testExamPage/testExam.php">Thi thử</a></li class="active">
+    <li> <a href="/GoPro_SE192/schedulePage/view/viewTkb.php">Thời khóa biểu</a></li>
+    <li> <a href="/GoPro_SE192/filePage/view/manageFile.php">Tài liệu</a></li>
+    <li> <a href="/GoPro_SE192/searchPage/">Tìm kiếm đề thi</a></li>
+    <li> <a href="/GoPro_SE192/forumPage/view/view_cat.php">Diễn đàn</a></li>
+</ul>

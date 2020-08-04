b8d67e4a4019ad126b65a7001561f82aae6df7d3
khiem20tc <53548789+khiem20tc@users.noreply.github.com>
2020-08-04 22:00:53 +0700

    update filePage

--- menuStudent.php
+++ menuStudent.php
@@ -2,7 +2,7 @@
     <li> <a href="/GoPro_SE192/index.php">Trang chủ</a> </li>
     <li> <a href="/GoPro_SE192/testExamPage/">Thi thử</a></li class="active">
     <li> <a href="/GoPro_SE192/schedulePage/view/viewTkb.php">Thời khóa biểu</a></li>
-    <li> <a href="/GoPro_SE192/filePage/view/manageFile.php">Tài liệu</a></li>
+    <li> <a href="/GoPro_SE192/filePage/view/listFile.php">Tài liệu</a></li>
     <li> <a href="/GoPro_SE192/searchPage/">Tìm kiếm đề thi</a></li>
     <li> <a href="/GoPro_SE192/forumPage/view/view_cat.php">Diễn đàn</a></li>
 </ul>

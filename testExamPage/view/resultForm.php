5c1db7a6ec10ae34e0950c527b9014ffa9e5e0bf
Ho Quang Khai <khaivcqk@gmail.com>
2020-08-05 23:57:11 +0700

    Update CSS testExamPage

--- testExamPage/view/resultForm.php
+++ testExamPage/view/resultForm.php
@@ -1,11 +1,12 @@
 <?php
 $result = $viewExam->ctrl->markingExam($_GET['subject'], $viewExam->listKey, $_POST);
-echo "Kết quả của bạn là:";
-echo "$result";
-echo "<br>";
+echo "<div id='result'>";
+echo "'<h3>Kết quả của bạn là: &nbsp; $result</h3>";
 echo "<a href='viewSuggestion.php'>Xem gợi ý</a><br>";
 echo "<a href='/GoPro_SE192/testExamPage/testExam.php'>Tiếp tục thi thử</a><br>";
 echo "<a href='/GoPro_SE192/index.php'>Về trang chủ</a><br>";
+echo "</div>";
+
 
 
 ?>
